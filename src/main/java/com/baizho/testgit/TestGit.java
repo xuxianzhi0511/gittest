2c8cd7cc3952b514bf440a25bceaa556d8979bda
xu <1814366205@qq.com>
2019-09-18 19:42:46 +0800

    5.0

--- src/main/java/com/baizho/testgit/TestGit.java
+++ src/main/java/com/baizho/testgit/TestGit.java
@@ -9,6 +9,7 @@ public class TestGit {
         System.out.println("test 4");
         System.out.println("test 5");
         System.out.println("test 6");
+        System.out.println("77777");
 
     }
 }
