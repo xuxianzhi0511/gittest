d6f48aecb816c10cb1d7b2381eb81dc862af8ac8
xu <1814366205@qq.com>
2019-09-18 17:15:46 +0800

    v1.0

--- src/main/java/com/baizho/testgit/TestGit.java
+++ src/main/java/com/baizho/testgit/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("test git ");
         System.out.println("test git ");
+        System.out.println("test git ");
     }
 }
